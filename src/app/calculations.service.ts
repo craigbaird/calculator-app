24e670d1af6192154c2eed6cf5733fa837246ca6
Craig Baird <bairdcraig@gmail.com>
2020-05-20 10:35:56 -0500

    config

--- src/app/calculations.service.ts
+++ src/app/calculations.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { httpFactory } from '@angular/http/src/http_module';
+import * as request from "request-promise-native";
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,19 @@ export class CalculationsService {
 
   getCalculations() {
     console.log('in getCalculations');
-    this.http.get('/calculation').subscribe(data => {
-      console.log('Data in client', data);
-      this.calculations = data;
-    })
+    // this.http.get('/calculation').subscribe(data => {
+    //   console.log('Data in client', data);
+    //   this.calculations = data;
+    // })
+    // (async () => {
+    //   const baseUrl = 'www.random.org/integers';
+    //   const queryString = '?num=100&min=1&max=100&col=5&base=10&format=html&rnd=new';
+    //   var options = {
+    //       uri: baseUrl + queryString,
+    //   };
+    
+    //   const result = await request.get(options);
+    //   console.log('result', result);
+    // })()
   }
 }
